16da956f2d89d68255082266180049ee4843b43b
fullstackmommy <nicole.budiman@gmail.com>
2019-02-22 11:51:59 +0800

    Use Lodash

--- src/components/Counters/Counters.jsx
+++ src/components/Counters/Counters.jsx
@@ -1,46 +1,64 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Counter from "../Counter/Counter";
+import {cloneDeep} from "lodash";
 
 export class Counters extends Component {
   state = {
     data: [
-      { id: 1, value: 0},
-      { id: 2, value: 0 }
+      {
+        id: 1,
+        value: 0
+      }, {
+        id: 2,
+        value: 0
+      }
     ]
   };
 
   settingState = (counterId, operator) => {
     const copy = [...this.state.data]
-    copy.find(element => element.id === counterId).value += operator;
-    this.setState({ data: copy });
+    copy
+      .find(element => element.id === counterId)
+      .value += operator;
+    this.setState({data: copy});
   }
+
   handleIncrement = (counterId) => {
     //TODO: To check article on async update of counter
-    this.settingState(counterId, 1)
+    const copy = cloneDeep(this.state.data)
+    // try using cloneDeep from lodash
+    console.log("Before", JSON.stringify(this.state.data));
+    copy
+      .find(element => element.id === counterId)
+      .value += 1;
+    console.log("After: state", JSON.stringify(this.state.data));
+    console.log("After: copy", JSON.stringify(copy));
+
+    this.setState({data: copy});
+
+    //this.settingState(counterId, 1)
   };
-  
+
   handleReset = () => {
     const copy = [...this.state.data]
-    const updated = copy.map( counter => {
+
+    const updated = copy.map(counter => {
       counter.value = 0;
       return counter;
     })
     this.setState({data: updated});
   }
   render() {
-    const { data } = this.state;
-    
+    const {data} = this.state;
+
     return (
       <div>
         <button onClick={this.handleReset} className="btn btn-secondary m-2">Reset</button>
-        { data.map(counter => (
-          <Counter
-            key={counter.id}
-            id={counter.id}
-            value={counter.value}
-            handleIncrement={this.handleIncrement}
-          />
-        ))}
+        {data.map(counter => (<Counter
+          key={counter.id}
+          id={counter.id}
+          value={counter.value}
+          handleIncrement={this.handleIncrement}/>))}
       </div>
     );
   }
